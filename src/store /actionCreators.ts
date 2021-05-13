1901cd19cb2d04c0d9362fd1ae0adaad2af0f54b
aarao <aarao.melo.lopes@gmail.com>
2021-05-13 19:35:17 -0300

    add action creators

---
+++ src/store /actionCreators.ts
@@ -0,0 +1,33 @@
+import * as actionTypes from "./actionTypes"
+
+export function addUser(user: IUser) {
+    const action: UserAction = {
+        type: actionTypes.ADD_USER,
+        user,
+    }
+    return simulateHttpRequest(action)
+}
+
+export function removeUser(user: IUser) {
+    const action: UserAction = {
+        type: actionTypes.REMOVE_USER,
+        user,
+    }
+    return simulateHttpRequest(action)
+}
+
+export function updateUser(user: IUser) {
+    const action: UserAction = {
+        type: actionTypes.UPDATE_USER,
+        user,
+    }
+    return simulateHttpRequest(action)
+}
+
+export function simulateHttpRequest(action: UserAction) {
+    return (dispatch: DispatchType) => {
+        setTimeout(() => {
+        dispatch(action)
+        }, 200)
+    }
+}
