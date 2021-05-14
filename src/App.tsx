6a461a33406ecddd12f84d3f584d529dd6d0c4f4
aarao <aarao.melo.lopes@gmail.com>
2021-05-13 22:14:07 -0300

    add app and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react"
+import { useSelector, shallowEqual, useDispatch } from "react-redux"
+import { User } from "./components/User"
+import { AddUser } from "./components/AddUser"
+import { addUser, removeUser } from "./store/actionCreators"
+import { Dispatch } from "redux"
+import "./App.css"
 
-function App() {
+const App: React.FC = () => {
+  const users: readonly IUser[] = useSelector(
+    (state: UserState) => state.users,
+    shallowEqual
+  )
+
+  const dispatch: Dispatch<any> = useDispatch()
+
+  const saveUser = React.useCallback(
+    (user: IUser) => dispatch(addUser(user)),
+    [dispatch]
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <h1>Usuários</h1>
+      <AddUser saveUser={saveUser} />
+      {users.map((user: IUser) => (
+        <User
+          key={user.cpf}
+          user={user}
+          removeUser={removeUser}
+        />
+      ))}
+    </main>
+  )
 }
 
-export default App;
+export default App
